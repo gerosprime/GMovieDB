99cfb97f99d33917368d60ee3dade347551671a1
glenn.ross.pascual <glennpascual011@gmail.com>
2018-11-12 18:28:57 +0800

    Project changes.
    - Added custom Application class.
    - Added essential third party libraries.
    - Changed Java version to 1.8

---
+++ app/src/main/java/com/gerosprime/gmoviedb/GMovieDBApplication.java
@@ -0,0 +1,12 @@
+package com.gerosprime.gmoviedb;
+
+import android.app.Application;
+
+public class GMovieDBApplication
+    extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
